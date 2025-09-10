5b68c398520c081322b9dc0bdeb36b201aa01fb4
Pomax <pomax@nihongoresources.com>
2025-09-09 17:08:18 -0700

    markdown

--- src/server/templating.js
+++ src/server/templating.js
@@ -1,5 +1,6 @@
 // Set up the render engine:
 import nunjucks from "nunjucks";
+import { marked } from "marked";
 import { safify, scrubDateTime } from "../helpers.js";
 
 export function setupTemplating(app) {
@@ -14,7 +15,15 @@ export function setupTemplating(app) {
   nenv.addFilter(`date`, (str, count) => scrubDateTime(str));
 
   nenv.addFilter(`dockerimg`, (str, count) =>
-    str.startsWith(`sha256`) ? `(hash only)` : str,
+    str.startsWith(`sha256`) ? `(hash only)` : str
+  );
+
+  nenv.addFilter(`markdown`, (str, count) =>
+    marked.parse(
+      str
+        .replaceAll(/<script/g, `<whatahack`)
+        .replaceAll(`</script`, `</whatahack`)
+    )
   );
 
   nenv.addFilter(`para`, (str, count) =>
@@ -22,7 +31,7 @@ export function setupTemplating(app) {
       ?.split(/\n/g)
       .filter(Boolean)
       .map((p) => `<p>${safify(p)}</p>`)
-      .join(`\n`),
+      .join(`\n`)
   );
 
   nenv.addFilter(`shorthash`, (str, count) => str.substring(0, 16));
